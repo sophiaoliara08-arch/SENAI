aa4985349f1bb96c102dd59451b1885494978c0c
unknown <sophiaoliara.08@gmail.com>
2026-02-20 16:16:48 -0300

    Atividades

--- Atividade6.java
+++ Atividade6.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Atividade6 {
 public static void main(String[] args) {
+	//ENTRADA
 	double raio, area, pi;
-	Scanner ler = new Scanner(System)
+	Scanner ler = new Scanner(System.in);
+	System.out.println("Informe o raio do objeto: ");
+	raio = ler.nextDouble();
+	System.out.println("Informe o PI considerado: ");
+	pi = ler.nextDouble();
+	//PROCESSAMENTO
+	area = ((raio*raio)*pi);
+	//SAIDA
+	System.out.println("o valor da àrea é: " + area);
+	
 }
 }
